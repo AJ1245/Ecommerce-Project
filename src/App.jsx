f3dadf4939737494b723c458beb9b757dcf59027
AJ1245 <abheerjasuja@gmail.com>
2025-11-01 23:41:44 -0400

    added checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
+import { CheckOutPage } from './pages/CheckOutPage.jsx'
+import { Routes,Route } from 'react-router'
 
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
+    <Routes>
+      <Route path='/' element={ <HomePage /> }> </Route>
+      <Route path='checkout' element={<CheckOutPage />}> </Route>
+    </Routes>
   )
 }
 
