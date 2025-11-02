b45e2bafac40ee00ae3e3de7a17e9b3849edd593
AJ1245 <abheerjasuja@gmail.com>
2025-11-02 00:53:28 -0400

    added orders and tracking page as static

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
 import { CheckOutPage } from './pages/CheckOutPage.jsx'
+import {OrdersPage} from './pages/OrdersPage.jsx'
+import { TrackingPage } from './pages/TrackingPage.jsx'
 import { Routes,Route } from 'react-router'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path='/' element={ <HomePage /> }> </Route>
       <Route path='checkout' element={<CheckOutPage />}> </Route>
+      <Route path='orders' element={<OrdersPage />}> </Route>
+      <Route path='tracking' element={<TrackingPage />}> </Route>
     </Routes>
   )
 }
